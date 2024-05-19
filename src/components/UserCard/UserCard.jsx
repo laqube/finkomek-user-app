b92ec963aa73bf7dc93ba5c210c8f5861c1676ea
Miras <91594617+laqube@users.noreply.github.com>
2024-05-19 15:27:16 +0500

    add user card
    -fixed pages layout
    -finished up the user card
    -will work on tabs then booking cards and expert catalogue card after I take a shower

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styles from "./usercard.module.css";
+import { Link } from "react-router-dom";
 
 const UserCard = () => {
   return (
     <div className={styles.user_card_body}>
+      <Link exact to="/user/settings" className={styles.user_icon_wrapper}>
+        <img
+          src="/assets/edit_icon.svg"
+          alt="edit"
+          className={styles.user_card_icon}
+        />
+      </Link>
       <img
         className={styles.user_card_pfp}
         alt="pfp"
         src="/assets/user_pfp.png"
       />
+      <h1 className={styles.user_name}>John Doe</h1>
+      <p className={styles.user_info}>exmaple@gmail.com</p>
+      <p className={styles.user_info}>87006005544</p>
     </div>
   );
 };
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -7,15 +7,13 @@ import UserTabs from "../../components/UserTabs/UserTabs";
 
 const UserPage = () => {
   return (
-    <div>
+    <div className={styles.page_wrapper}>
       <Navigation />
       <div className={styles.page_container}>
         <div className={styles.page_column}>
           <UserCard />
         </div>
-        <div className={styles.page_column}>
-          <UserTabs />
-        </div>
+        <div className={styles.page_column}>User Tabs</div>
       </div>
       <Footer />
     </div>
