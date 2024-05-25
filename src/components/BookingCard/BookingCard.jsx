4d3211287f0e9c83a14474f979b253aff742bbc3
Miras <91594617+laqube@users.noreply.github.com>
2024-05-25 12:49:45 +0500

    New Componennt
    -Booking card component layout is done, this component is for showing a user all the booked meetings with experts on the user's page
    -next is to develop is the experts and calculator catalogues
    -tomorrow will work on the backend connection, mapping logic and etc. today I'll just do all as much layouts as I can

--- src/components/BookingCard/BookingCard.jsx
+++ src/components/BookingCard/BookingCard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "./bookingcard.module.css";
 
 const BookingCard = () => {
-  return <div>BookingCard</div>;
+  return (
+    <div className={styles.bookingcard_container}>
+      <h1 className={styles.bc_header}>Күнделікті консультация</h1>
+
+      <div className={styles.bc_date_wrapper}>
+        <img
+          className={styles.bc_date_icon}
+          src="/assets/bc_calendar_icon.svg"
+        />
+        <p className={styles.bc_date} style={{ margin: "0" }}>
+          20.06.2024
+        </p>
+      </div>
+
+      <div className={styles.bc_name_row}>
+        <div className={styles.bc_name_wrapper}>
+          <img className={styles.bc_name_icon} src="/assets/bc_name_icon.svg" />
+          <p style={{ margin: "0" }}>Әйгүл Сәрсен</p>
+        </div>
+        <img className={styles.bc_chat_icon} src="/assets/bc_chat_icon.svg" />
+      </div>
+    </div>
+  );
 };
 
 export default BookingCard;
--- src/components/UserTabs/UserTabs.jsx
+++ src/components/UserTabs/UserTabs.jsx
@@ -3,49 +3,19 @@ import ReactDOM from "react-dom";
 import styles from "./usertabs.module.css";
 import UserTabs_Tabs from "./UserTabs_Tabs";
 import UserTabs_Panel from "./UserTabs_Panel";
+import BookingCard from "../BookingCard/BookingCard";
 
 const UserTabs = () => {
   return (
     <div className={styles.user_tabs_body}>
       <UserTabs_Tabs>
         <UserTabs_Panel title="Күнтізбе">
-          Panel 1 <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
         </UserTabs_Panel>
         <UserTabs_Panel title="Курстар">
           Panel 2 <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-          Test <br />
-        </UserTabs_Panel>
-        <UserTabs_Panel title="Мұрағат">
-          Panel 3 <br />
           Test <br />
           Test <br />
           Test <br />
