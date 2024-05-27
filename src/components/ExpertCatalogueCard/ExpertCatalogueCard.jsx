4f02a7e90c2fa5f7bf285f5646b434645ec8417b
Miras <91594617+laqube@users.noreply.github.com>
2024-05-27 21:52:14 +0500

    add fetch experts

--- src/components/ExpertCatalogueCard/ExpertCatalogueCard.jsx
+++ src/components/ExpertCatalogueCard/ExpertCatalogueCard.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import styles from "./expertcataloguecard.module.css";
 
 const ExpertCatalogueCard = ({ item }) => {
-  const { name, url, experience } = item;
+  const { firstName, lastName, imageLink, description } = item;
   return (
     <div className={styles.ecard}>
       <div className={styles.ecard_pic_container}>
-        <img alt="img" src={url} className={styles.ecard_pic} />
+        <img alt="img" src={imageLink} className={styles.ecard_pic} />
       </div>
       <div className={styles.ecard_text_container}>
-        <h1 className={styles.ecard_name}>{name}</h1>
-        <p className={styles.ecard_desc}>{experience}</p>
+        <h1 className={styles.ecard_name}>
+          {firstName} {lastName}
+        </h1>
+        <p className={styles.ecard_desc}>{description}</p>
         <button className={styles.ecard_button}>Өту</button>
       </div>
     </div>
--- src/pages/ExpertsCatalogue/ExpertsCatalogue.jsx
+++ src/pages/ExpertsCatalogue/ExpertsCatalogue.jsx
@@ -1,37 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./expertscatalogue.module.css";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
 import ExpertCatalogueCard from "../../components/ExpertCatalogueCard/ExpertCatalogueCard";
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const ExpertsCatalogue = () => {
-  const [experts, setExperts] = useState([
-    {
-      name: "Zaur",
-      url: "https://thumbs.dreamstime.com/b/boy-brushing-his-teeth-electric-tooth-brush-stock-image-grey-background-photo-little-258821294.jpg",
-      experience: "2 years",
-    },
-    {
-      name: "Miras",
-      url: "https://img.freepik.com/premium-photo/hd-wallpaper-stock-image_783884-41694.jpg",
-      experience: "3 years",
-    },
-    {
-      name: "Kamal",
-      url: "https://static3.bigstockphoto.com/5/8/2/large1500/285287722.jpg",
-      experience: "29 years",
-    },
-    {
-      name: "Abo",
-      url: "https://static.vecteezy.com/system/resources/previews/027/548/110/non_2x/old-man-with-thumbs-up-ai-generative-photo.jpg",
-      experience: "0.5 years",
-    },
-    {
-      name: "Umar",
-      url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2020%2F04%2F09%2F05%2F12%2Flaughing-5019740_1280.jpg&f=1&nofb=1&ipt=72f5f44b4630e92bebd3e28a49dffca10c8477b84b7345e50acd1db1aa4a0ada&ipo=images",
-      experience: "35 years",
-    },
-  ]);
+  const [experts, setExperts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${apiKey}/expert/getAllExperts`).then((response) => {
+      setExperts(response.data.experts);
+    });
+  }, []);
 
   return (
     <div className={styles.page_wrapper}>
