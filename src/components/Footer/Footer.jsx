dd7d5372731be9dfb53dc3bf55e63a9849b80da6
Miras <91594617+laqube@users.noreply.github.com>
2024-05-30 13:45:35 +0500

    update footer
    -footer redesign
    -fixed navigation height being different on certain pages
    -added invisible scrollbar so the default is not applied to the pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footer_container}>
-      <div className={styles.footer_logo_wrapper}>
-        <img src="/assets/logo.png" alt="logo" className={styles.footer_logo} />
-      </div>
-      <p>Осыны Мирас Жазған</p>
+      <a className={styles.content_container} href="https://github.com/laqube">
+        <img className={styles.github_icon} src="/assets/github-mark.svg" />
+        <p className={styles.footer_text}>Developed by laqube</p>
+      </a>
     </div>
   );
 };
