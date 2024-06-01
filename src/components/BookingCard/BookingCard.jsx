bff99601e55283616de415cb57409e5d9253b62f
Miras <91594617+laqube@users.noreply.github.com>
2024-06-02 01:52:37 +0500

    update booking card

--- src/components/BookingCard/BookingCard.jsx
+++ src/components/BookingCard/BookingCard.jsx
@@ -4,24 +4,31 @@ import styles from "./bookingcard.module.css";
 const BookingCard = () => {
   return (
     <div className={styles.bookingcard_container}>
-      <h1 className={styles.bc_header}>Күнделікті консультация</h1>
-
-      <div className={styles.bc_date_wrapper}>
-        <img
-          className={styles.bc_date_icon}
-          src="/assets/bc_calendar_icon.svg"
-        />
-        <p className={styles.bc_date} style={{ margin: "0" }}>
-          20.06.2024
-        </p>
-      </div>
-
-      <div className={styles.bc_name_row}>
-        <div className={styles.bc_name_wrapper}>
-          <img className={styles.bc_name_icon} src="/assets/bc_name_icon.svg" />
-          <p style={{ margin: "0" }}>Әйгүл Сәрсен</p>
+      <div className={styles.bc_col_info}>
+        <h1 className={styles.bc_info_name}>Expert name</h1>
+        <p className={styles.bc_info_topic}>Consultaion topic</p>
+        <div className={styles.bc_row_info}>
+          <div className={styles.bc_info_item}>
+            <img
+              alt="icon"
+              className={styles.bc_info_icon}
+              src="/assets/bc_calendar_icon.svg"
+            />
+            <p className={styles.bc_info_detail}>date</p>
+          </div>
+          <div className={styles.bc_info_item}>
+            <img
+              alt="icon"
+              className={styles.bc_info_icon}
+              src="/assets/bc_clock_icon.svg"
+            />
+            <p className={styles.bc_info_detail}>time</p>
+          </div>
         </div>
-        <img className={styles.bc_chat_icon} src="/assets/bc_chat_icon.svg" />
+      </div>
+      <div className={styles.bc_col_buttons}>
+        <button className={styles.bc_button_yes}>Қосылу</button>
+        <button className={styles.bc_button_no}>Бас тарту</button>
       </div>
     </div>
   );
