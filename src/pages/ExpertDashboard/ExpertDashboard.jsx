4974f9558e52278b41a8e542b4b056430899d55e
Miras <91594617+laqube@users.noreply.github.com>
2024-06-08 22:57:04 +0500

    update expert dashboard
    -fetch expert data

--- src/pages/ExpertDashboard/ExpertDashboard.jsx
+++ src/pages/ExpertDashboard/ExpertDashboard.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./expertdashboard.module.css";
+import { useTranslation } from "react-i18next";
+import { API } from "../../api";
 import Navigation from "../../components/Navigation/Navigation";
 import ExpertNavigation from "../../components/ExpertNavigation/ExpertNavigation";
 import Footer from "../../components/Footer/Footer";
-import { useTranslation } from "react-i18next";
 
 const ExpertDashboard = () => {
   const { t } = useTranslation("translation");
+  const [expert, setExpert] = useState({});
+  useEffect(() => {
+    API.get(`/expert/get`)
+      .then((response) => {
+        // console.log(response.data.expert);
+        setExpert(response.data.expert);
+      })
+      .catch((error) => {
+        console.error("Error fetching course:", error);
+      });
+  }, []);
   return (
     <div className={styles.page_wrapper}>
       <ExpertNavigation />
@@ -14,24 +26,29 @@ const ExpertDashboard = () => {
         <div className={styles.expert_container}>
           <div className={styles.expert_box_container}>
             <div className={styles.expert_box}>
-              <img className={styles.expert_avi} alt="avi" src="/" />
-              <h1 className={styles.expert_name}>Name Surname</h1>
-              <p className={styles.expert_email}>example@gmail.com</p>
+              <img
+                className={styles.expert_avi}
+                alt="avi"
+                src={expert.imageLink}
+              />
+              <h1 className={styles.expert_name}>
+                {expert.firstName} {expert.lastName}
+              </h1>
+              <p className={styles.expert_email}> {expert.email} </p>
               <p className={styles.expert_price}>
-                price{t("page_expert.tag_price")}{" "}
+                {expert.cost}
+                {t("page_expert.tag_price")}{" "}
               </p>
-              <p className={styles.expert_desc}>
-                Финтехта 12 жыл тәжірибем бар. Білікті және өте күшті маманмын.
-                КөптегенФинтехта 12 жыл тәжірибем бар. Білікті және өте күшті
-                маманмын. Көптеген
-              </p>
+              <p className={styles.expert_desc}>{expert.description}</p>
             </div>
           </div>
           <div className={styles.expert_calendar_container}>
-            <h1 className={styles.calendar_heading}>{t("expert.heading")}</h1>
+            <h1 className={styles.calendar_heading}>
+              {t("expert.heading")} {expert.firstName} !
+            </h1>
             <div className={styles.calendar_create_row}>
               <h1 className={styles.calendar_create_heading}>
-                {t("expert.msg")}{" "}
+                {t("expert.msg")}
               </h1>
               <button className={styles.calendar_create_button}>
                 {t("expert.button_create")}{" "}
